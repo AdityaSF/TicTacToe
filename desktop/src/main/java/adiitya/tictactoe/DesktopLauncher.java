90355ff0aa05153d8b221003a2fcc6817af1979e
AdityaSF <A44itya@hotmail.com>
2019-10-13 16:53:59 -0600

    Rename desktop and HTML launchers from TicTacToeDesktop to DesktopLauncher and TicTacToeHtml to HtmlLauncher

--- desktop/src/main/java/adiitya/tictactoe/DesktopLauncher.java
+++ desktop/src/main/java/adiitya/tictactoe/DesktopLauncher.java
@@ -3,7 +3,7 @@ package adiitya.tictactoe;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
-public class TicTacToeDesktop {
+public class DesktopLauncher {
 	public static void main (String[] args) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new TicTacToe(), config);
