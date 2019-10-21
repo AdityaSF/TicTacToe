40f8b0a419e30a8bcfde29bc7e2e906cfc40f67c
AdityaSF <A44itya@hotmail.com>
2019-10-21 17:46:10 -0600

    Convert ticTacToe to Game and move rendering logic to PlayScreen

---
+++ core/src/main/java/adiitya/tictactoe/PlayScreen.java
@@ -0,0 +1,124 @@
+package adiitya.tictactoe;
+
+import adiitya.tictactoe.Cell.CellType;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.Array;
+
+public class PlayScreen implements Screen, InputProcessor {
+
+	private final TicTacToe ttt;
+
+	private TextureRegion dividers;
+	private Array<Cell> cells;
+
+	private PlayerType playerType;
+
+	public PlayScreen(TicTacToe ttt) {
+		this.ttt = ttt;
+	}
+
+	@Override
+	public void show() {
+
+		dividers = ttt.atlas.findRegion("board_dividers");
+
+		cells = new Array<>();
+		cells.add(new Cell(117, 109, CellType.CORNER));
+		cells.add(new Cell(211, 109, CellType.TOP));
+		cells.add(new Cell(297, 109, CellType.CORNER));
+		cells.add(new Cell(117, 211, CellType.SIDE));
+		cells.add(new Cell(211, 211, CellType.CENTER));
+		cells.add(new Cell(297, 211, CellType.SIDE));
+		cells.add(new Cell(117, 305, CellType.CORNER));
+		cells.add(new Cell(211, 305, CellType.TOP));
+		cells.add(new Cell(297, 305, CellType.CORNER));
+
+		playerType = PlayerType.X;
+
+		Gdx.input.setInputProcessor(this);
+	}
+
+	@Override
+	public void render(float delta) {
+
+		float scale = TicTacToe.SCALE;
+
+		ttt.batch.draw(dividers, 14F * scale, 13F * scale, 35F * scale, 38F * scale);
+		cells.forEach(c -> c.render(ttt.batch));
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// resize logic is handled in TicTacToe
+	}
+
+	@Override
+	public void pause() {
+		// unused in desktop
+	}
+
+	@Override
+	public void resume() {
+		// unused in desktop
+	}
+
+	@Override
+	public void hide() {
+		// unused in desktop
+	}
+
+	@Override
+	public void dispose() {
+		// no disposable resources
+	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+
+		for (Cell cell : cells) {
+
+			if (cell.onClick(screenX, screenY, playerType))
+				playerType = PlayerType.flip(playerType);
+		}
+
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		return false;
+	}
+}
--- core/src/main/java/adiitya/tictactoe/TicTacToe.java
+++ core/src/main/java/adiitya/tictactoe/TicTacToe.java
@@ -1,33 +1,20 @@
 package adiitya.tictactoe;
 
-import adiitya.tictactoe.Cell.CellType;
-import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.utils.Array;
 
 import static com.badlogic.gdx.graphics.GL20.*;
 
-public class TicTacToe implements ApplicationListener, InputProcessor {
+public class TicTacToe extends Game {
 
-	OrthographicCamera cam;
-	TextureAtlas atlas;
-	SpriteBatch batch;
+	public OrthographicCamera cam;
+	public TextureAtlas atlas;
+	public SpriteBatch batch;
 
-	TextureRegion dividers;
-	Array<Cell> cells;
-	Animation<AtlasRegion> oAnim;
-	Animation<AtlasRegion> xAnim;
-	float elapsed = 0;
-	public float SCALE;
-
-	PlayerType playerType;
+	public static final float SCALE = 500F / 64F;
 
 	@Override
 	public void create () {
@@ -36,28 +23,7 @@ public class TicTacToe implements ApplicationListener, InputProcessor {
 		atlas = new TextureAtlas(Gdx.files.internal("game.atlas"), true);
 		batch = new SpriteBatch();
 
-		dividers = atlas.findRegion("board_dividers");
-
-		cells = new Array<>();
-		cells.add(new Cell(117, 109, CellType.CORNER));
-		cells.add(new Cell(211, 109, CellType.TOP));
-		cells.add(new Cell(297, 109, CellType.CORNER));
-		cells.add(new Cell(117, 211, CellType.SIDE));
-		cells.add(new Cell(211, 211, CellType.CENTER));
-		cells.add(new Cell(297, 211, CellType.SIDE));
-		cells.add(new Cell(117, 305, CellType.CORNER));
-		cells.add(new Cell(211, 305, CellType.TOP));
-		cells.add(new Cell(297, 305, CellType.CORNER));
-
-		Array<AtlasRegion> oFrames = atlas.findRegions("o");
-		Array<AtlasRegion> xFrames = atlas.findRegions("x");
-		oAnim = new Animation<>(1F / oFrames.size, oFrames, Animation.PlayMode.LOOP);
-		xAnim = new Animation<>(1F / xFrames.size, xFrames, Animation.PlayMode.LOOP);
-
-		playerType = PlayerType.X;
-		SCALE = 500F / 64F;
-
-		Gdx.input.setInputProcessor(this);
+		setScreen(new PlayScreen(this));
 	}
 
 	@Override
@@ -76,18 +42,9 @@ public class TicTacToe implements ApplicationListener, InputProcessor {
 		Gdx.gl.glClearColor(0.149F, 0.169F, 0.267F, 0);
 		Gdx.gl.glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		elapsed += Gdx.graphics.getDeltaTime();
-
 		batch.begin();
-		batch.draw(dividers, 14F * SCALE, 13F * SCALE, 35F * SCALE, 38F * SCALE);
-		cells.forEach(c -> c.render(batch));
+		getScreen().render(Gdx.graphics.getDeltaTime());
 		batch.end();
-	}
-
-	private void drawAnimation(SpriteBatch batch, Animation<AtlasRegion> anim, float x, float y, float elapsed, boolean flip) {
-
-		AtlasRegion texture = anim.getKeyFrame(elapsed);
-		RenderUtils.renderFlipable(batch, texture, x, y, texture.getRegionWidth(), texture.getRegionHeight(), SCALE, flip);
 	}
 
 	@Override
@@ -104,52 +61,5 @@ public class TicTacToe implements ApplicationListener, InputProcessor {
 	public void dispose () {
 		atlas.dispose();
 		batch.dispose();
-	}
-
-	@Override
-	public boolean keyDown(int keycode) {
-		return false;
-	}
-
-	@Override
-	public boolean keyUp(int keycode) {
-		return false;
-	}
-
-	@Override
-	public boolean keyTyped(char character) {
-		return false;
-	}
-
-	@Override
-	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-		return false;
-	}
-
-	@Override
-	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-
-		for (Cell cell : cells) {
-
-			if (cell.onClick(screenX, screenY, playerType))
-				playerType = PlayerType.flip(playerType);
-		}
-
-		return false;
-	}
-
-	@Override
-	public boolean touchDragged(int screenX, int screenY, int pointer) {
-		return false;
-	}
-
-	@Override
-	public boolean mouseMoved(int screenX, int screenY) {
-		return false;
-	}
-
-	@Override
-	public boolean scrolled(int amount) {
-		return false;
 	}
 }
